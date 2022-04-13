8d94db6782a9a1ee85a68c9ce1e9ff6d203d8b5a
Rafael Cicaroli <95261367+rafaelcicaroli@users.noreply.github.com>
2022-04-13 12:58:26 -0300

    Add files via upload

---
+++ quotes/quotes/spiders/quotes_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+def clean_text(text):
+    text = text.strip(u'\u201c')
+    text = text.strip(u'\u201d')
+    return text
+
+class QuotesSpider(scrapy.Spider):
+     name = "quotes"
+     start_urls = [
+        "http://quotes.toscrape.com/page/1/"
+     ]
+
+     def parse(self, response):
+         for quote in response.css('div.quote'):
+             yield {
+                'text': clean_text(quote.css('span.text::text').get()),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall()
+             }
+         next_page = response.css('li.next a::attr(href)').get()
+         if next_page is not None:
+             yield response.follow(next_page, callback=self.parse)
